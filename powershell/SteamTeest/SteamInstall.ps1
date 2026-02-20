41a29b040c242c18f71e7500643d0f4678a30294
ParanoidAndroid-AFK <b.edward.lane@gmail.com>
2026-02-19 20:41:28 -0500

    attempts to create a catch output file

--- powershell/SteamTeest/SteamInstall.ps1
+++ powershell/SteamTeest/SteamInstall.ps1
@@ -3,6 +3,9 @@ $TempDir = Join-Path $env:USERPROFILE "Downloads"
 $SteamInstaller = Join-Path $TempDir "SteamSetup.exe"
 $SteamURL = "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe"
 Invoke-WebRequest -Uri $SteamURL -OutFile $SteamInstaller 
-Start-process -FilePath $SteamInstaller -ArgumentList "/S" -Wait -NoNewWindow
-
-
+Try {
+    Start-process -FilePath $SteamInstaller -ArgumentList "/S" -Wait -NoNewWindow
+}
+Catch {
+    Set-Content -Path "$env:USERPROFILE\Downloads\CatchTest.txt" -Value $_.Exception.Message
+}
